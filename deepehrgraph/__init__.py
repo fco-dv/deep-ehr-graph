93eb9e6969be9927f29f40c6c16658e62e728eac
Francois Cokelaer <francois.cokelaer@gmail.com>
2023-11-11 13:04:32 +0100

    pkg version test

--- deepehrgraph/__init__.py
+++ deepehrgraph/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution('deepehrgraph').version
+
 
 class DeepEhrGraph:
     def __init__(self) -> None:
---
+++ tests/deepehrgraph_test.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from deepehrgraph import __version__
+
+
+def test_version():
+    assert __version__ == '0.2.0'
